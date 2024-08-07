eac703ec6503d194466aca313675e24c516a006f
tianpratama1109 <tianpratama1109@gmail.com>
2024-08-07 14:59:55 +0000

    first commit

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x8C98664c3fae5CEb926C51b0411218ceD4C86dbC'
+export const ProxyAdmin = '0xaB41f6622CA48f3dAf3Fc4C4d1E0f7B017157CC9'
+export const SWTRImplementation = '0x8401F8aE61901d7ea1c726983A0bA857cDeF2F26'
+    
